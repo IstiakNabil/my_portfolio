f473d714e23d7700079c87ae1f2087c060787d19
IstiakNabil <128123853+IstiakNabil@users.noreply.github.com>
2026-05-21 05:19:18 +0600

    Fixed mobile navbar and responsive UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,13 @@ export default function Navbar() {
   return (
     <header className="sticky-header">
       <nav>
+        <div className="logo">Muhammad Nabil</div>
         {/* 1. The Burger Button: Clicking it flips the switch */}
         <div className="burger" onClick={() => setIsOpen(!isOpen)}>
           <span></span>
           <span></span>
           <span></span>
         </div>
-        
-        <div className="logo">Muhammad Nabil</div>
         
         {/* 2. The Links: If isOpen is true, it adds the "active" class */}
         <div className={`nav-links ${isOpen ? 'active' : ''}`}>
